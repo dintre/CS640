5eacb7135997bcca81376b208b60f9834503474a
Trevor <43910691+dintre@users.noreply.github.com>
2019-11-02 19:10:36 -0500

    Create myrouter_part2.py

---
+++ myrouter_part2.py
@@ -0,0 +1,64 @@
+
+#!/usr/bin/env python3
+'''
+Basic IPv4 router (static routing) in Python.
+'''
+import sys
+import os
+import time
+from switchyard.lib.packet.util import *
+from switchyard.lib.userlib import *
+
+class Router(object):
+
+
+    def __init__(self, net):
+        self.net = net
+        # other initialization stuff here
+        self.arp_table = {} #first initialize empty ARP table for IP-MAC pairs
+        self.my_interfaces = net.interfaces()
+    def router_main(self):    
+        '''
+        Main method for router; we stay in a loop in this method, receiving
+        packets until the end of time.
+        '''
+        while True:
+            gotpkt = True
+            try:
+                timestamp,input_port,pkt = self.net.recv_packet(timeout=1.0)
+            except NoPackets:
+                log_debug("No packets available in recv_packet")
+                gotpkt = False
+            except Shutdown:
+                log_debug("Got shutdown signal")
+                break
+            if gotpkt:
+                log_debug("Got a packet: {}".format(str(pkt)))
+
+            if pkt.has_header(Arp):
+                matched = False
+                arpPkt = pkt[Arp]
+                for interface in self.my_interfaces:
+                    if interface.ipaddr == arpPkt.targetprotoaddr:
+                        matched = True
+                        break
+
+                if matched == True:
+                    #store in ARP table
+                    self.arp_table[arpPkt.senderprotoaddr] = arpPkt.senderhwaddr
+                    #send ARP reply
+                    targetEth = arpPkt.senderhwaddr
+                    targetIP = arpPkt.senderprotoaddr
+                    sourceIP = arpPkt.targetprotoaddr
+                    sourceEth = interface.ethaddr
+                    arpReply = create_ip_arp_reply(sourceEth,targetEth,sourceIP,targetIP)
+                    self.net.send_packet(input_port,arpReply) 
+def main(net):
+    '''
+    Main entry point for router.  Just create Router
+    object and get it going.
+    '''
+    r = Router(net)
+    r.router_main()
+    net.shutdown()
+
