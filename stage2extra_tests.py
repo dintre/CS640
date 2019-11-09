5a2ac84003ed5488abd31be3958015cb3e66b9ac
Trevor <43910691+dintre@users.noreply.github.com>
2019-11-08 18:36:30 -0600

    Create stage2extra_tests.py

---
+++ stage2extra_tests.py
@@ -0,0 +1,67 @@
+import struct
+
+from ipaddress import IPv4Address
+from switchyard.lib.userlib import *
+from switchyard.lib.packet import *
+
+
+def mk_dynamic_routing_packet(ethdst, advertised_prefix, advertised_mask,
+                               next_hop):
+    drm = DynamicRoutingMessage(advertised_prefix, advertised_mask, next_hop)
+    Ethernet.add_next_header_class(EtherType.SLOW, DynamicRoutingMessage)
+    pkt = Ethernet(src='00:00:22:22:44:44', dst=ethdst,
+                   ethertype=EtherType.SLOW) + drm
+    xbytes = pkt.to_bytes()
+    p = Packet(raw=xbytes)
+    return p
+
+def mk_pkt(hwsrc, hwdst, ipsrc, ipdst, reply=False, ttl = 64):
+    ether = Ethernet(src=hwsrc, dst=hwdst, ethertype=EtherType.IP)
+    ippkt = IPv4(src=ipsrc, dst=ipdst, protocol=IPProtocol.ICMP, ttl=ttl)
+    icmppkt = ICMP()
+    if reply:
+        icmppkt.icmptype = ICMPType.EchoReply
+    else:
+        icmppkt.icmptype = ICMPType.EchoRequest
+    return ether + ippkt + icmppkt
+
+
+def router_tests():
+    s = TestScenario("Basic functionality testing for DynamicRoutingMessage")
+
+    # Initialize switch with 3 ports.
+    s.add_interface('router-eth0', '10:00:00:00:00:01', ipaddr = '192.168.1.1', netmask = '255.255.255.252')
+    s.add_interface('router-eth1', '10:00:00:00:00:02', ipaddr = '10.10.0.1', netmask = '255.255.0.0')
+    s.add_interface('router-eth2', '10:00:00:00:00:03', ipaddr = '172.16.42.1', netmask = '255.255.255.0')
+
+
+    # 0   IP packet to be forwarded to 172.16.42.2 should arrive on
+    #     router-eth0
+    #         Expected event: recv_packet Ethernet
+    #         10:00:00:00:00:03->30:00:00:00:00:01 IP | IPv4
+    #         192.168.1.100->172.16.42.2 ICMP | ICMP EchoRequest 0 42 (0
+    #         data bytes) on router-eth0
+
+    #packet = mk_pkt(hwsrc = '10:00:00:00:00:03', hwdst =  '30:00:00:00:00:01', ipsrc  = '192.168.1.100', ipdst = '172.16.42.2')
+    #s.expect(PacketInputEvent("router-eth0", packet), "IP packet to be forwarded to 172.16.42.2 should arrive on router-eth0")
+
+    # 1 packet matches own interface
+    packet = mk_pkt(hwsrc = '10:00:00:00:00:03', hwdst =  '30:00:00:00:00:01', ipsrc  = '192.168.1.100', ipdst = '172.16.42.1')
+    s.expect(PacketInputEvent("router-eth0", packet), "IP packet to be forwarded to 172.16.42.1 should arrive on router-eth0")
+
+    #2 packet doesn't match anything in the forwarding table
+    packet = mk_pkt(hwsrc = '10:00:00:00:00:03', hwdst =  '30:00:00:00:00:01', ipsrc  = '192.168.1.100', ipdst = '172.19.42.1')
+    s.expect(PacketInputEvent("router-eth0", packet), "IP packet to be forwarded to 172.19.42.1 should arrive on router-eth0")
+
+    #3 TODO - make a packet that matches in fTable. Needs ARP request. tries 3 times and stops
+    packet = mk_pkt(hwsrc = '10:00:00:00:00:03', hwdst =  '30:00:00:00:00:01', ipsrc  = '192.168.1.100', ipdst = '172.19.42.1')
+    s.expect(PacketInputEvent("router-eth0", packet), "IP packet to be forwarded to 172.19.42.1 should arrive on router-eth0")
+
+
+
+    #s.expect(PacketInputTimeoutEvent(10), "Waiting 10 seconds")
+
+
+    return s
+
+scenario = router_tests()
