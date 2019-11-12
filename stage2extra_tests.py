ccd9c7497f4b9da8dafdfe4be420b56a36c1f666
Trevor <43910691+dintre@users.noreply.github.com>
2019-11-12 05:39:09 -0600

    Update stage2extra_tests.py

--- stage2extra_tests.py
+++ stage2extra_tests.py
@@ -35,6 +35,8 @@ def router_tests():
     s.add_interface('router-eth2', '10:00:00:00:00:03', ipaddr = '172.16.42.1', netmask = '255.255.255.0')
 
 
+
+
     # 0   IP packet to be forwarded to 172.16.42.2 should arrive on
     #     router-eth0
     #         Expected event: recv_packet Ethernet
@@ -58,7 +60,6 @@ def router_tests():
     s.expect(PacketInputEvent("router-eth0", packet), "IP packet to be forwarded to 172.19.42.2 should arrive on router-eth0")
 
     #3.2
-    #         out router-eth2
     arp_request  = create_ip_arp_request('10:00:00:00:00:03', '172.16.42.1', '172.16.42.2')
     s.expect(PacketOutputEvent("router-eth2", arp_request), "Router should send ARP request for 172.16.42.2 out router-eth2 interface")
 
@@ -76,7 +77,51 @@ def router_tests():
     #arp_request  = create_ip_arp_request('10:00:00:00:00:03', '172.16.42.1', '172.16.42.2')
     s.expect(PacketOutputEvent("router-eth2", arp_request), "Router should send ARP request for 172.16.42.2 out router-eth2 interface")
 
+    #s.expect(PacketInputTimeoutEvent(3), "Waiting 3 seconds")
 
+
+#----------------------------------------------------------
+
+    # 1   IP packet to be forwarded to 172.16.42.2 should arrive on
+    #     router-eth0
+    #         Expected event: recv_packet Ethernet
+    #         10:00:00:00:00:03->30:00:00:00:00:01 IP | IPv4
+    #         192.168.1.100->172.16.42.2 ICMP | ICMP EchoRequest 0 42 (0
+    #         data bytes) on router-eth0
+
+    packet = mk_pkt(hwsrc = '10:00:00:00:00:03', hwdst =  '30:00:00:00:00:01', ipsrc  = '192.168.1.100', ipdst = '172.16.42.2')
+    s.expect(PacketInputEvent("router-eth0", packet), "IP packet to be forwarded to 172.16.42.2 should arrive on router-eth0")
+
+    # 2   Router should send ARP request for 172.16.42.2 out router-
+    #     eth2 interface
+    #         Expected event: send_packet(s) Ethernet
+    #         10:00:00:00:00:03->ff:ff:ff:ff:ff:ff ARP | Arp
+    #         10:00:00:00:00:03:172.16.42.1 ff:ff:ff:ff:ff:ff:172.16.42.2
+    #         out router-eth2
+
+    arp_request  = create_ip_arp_request('10:00:00:00:00:03', '172.16.42.1', '172.16.42.2')
+    s.expect(PacketOutputEvent("router-eth2", arp_request), "Router should send ARP request for 172.16.42.2 out router-eth2 interface")
+
+    # 3   Router should receive ARP response for 172.16.42.2 on
+    #     router-eth2 interface
+    #         Expected event: recv_packet Ethernet
+    #         30:00:00:00:00:01->10:00:00:00:00:03 ARP | Arp
+    #         30:00:00:00:00:01:172.16.42.2 10:00:00:00:00:03:172.16.42.1
+    #         on router-eth2
+
+    arp_response = create_ip_arp_reply('30:00:00:00:00:01', '10:00:00:00:00:03',
+                                       '172.16.42.2', '172.16.42.1')
+    s.expect(PacketInputEvent("router-eth2", arp_response), "Router should receive ARP response for 172.16.42.2 on router-eth2 interface")
+
+
+    # 4   IP packet should be forwarded to 172.16.42.2 out router-eth2
+    #         Expected event: send_packet(s) Ethernet
+    #         10:00:00:00:00:03->30:00:00:00:00:01 IP | IPv4
+    #         192.168.1.100->172.16.42.2 ICMP | ICMP EchoRequest 0 42 (0
+    #         data bytes) out router-eth2
+
+    packet = mk_pkt(hwsrc='10:00:00:00:00:03', hwdst='30:00:00:00:00:01', ipsrc='192.168.1.100', ipdst='172.16.42.2', ttl=63)
+    s.expect(PacketOutputEvent("router-eth2", packet), "IP packet should be forwarded to 172.16.42.2 out router-eth2")
 
 
     return s
