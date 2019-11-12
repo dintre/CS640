733bf4cf61a77ae654d1154addb8e6f0d65426a9
Trevor <43910691+dintre@users.noreply.github.com>
2019-11-11 20:13:55 -0600

    Update myrouter_part2.py

--- myrouter_part2.py
+++ myrouter_part2.py
@@ -62,6 +62,7 @@ class Router(object):
             except NoPackets:
                 log_debug("No packets available in recv_packet")
                 gotpkt = False
+                continue
             except Shutdown:
                 log_debug("Got shutdown signal")
                 break
